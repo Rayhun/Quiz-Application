ec3dc730943ac8f61645de47559117c6b3ebb4df
rayhun <rayhunkhan27@gmail.com>
2022-11-07 18:30:14 +0600

    add template

--- quiz/views.py
+++ quiz/views.py
@@ -1,8 +1,30 @@
+# Python import
+import random
 # django import
 from django.shortcuts import render
 from django.views.generic import TemplateView
+# local import
+from .models import Question
+
+
+# class HomeView(TemplateView):
+#     template_name = 'home.html'
+
+#     def get_context_data(self, **kwargs):
+#         context = super(HomeView, self).get_context_data(**kwargs)
 
 
 class QuizApplicationView(TemplateView):
     """ Quiz Application Home page """
     template_name = 'quiz/quiz_application.html'
+    model = Question
+
+    def get_context_data(self, **kwargs):
+        context = super(QuizApplicationView, self).get_context_data(**kwargs)
+        qs = list(self.model.objects.filter(is_active=True))
+        random.shuffle(qs)
+        context['object_list'] = qs
+        context['total_quiz'] = self.model.objects.filter(
+            is_active=True
+        ).count()
+        return context
