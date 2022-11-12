856aeb6dbaf54c17f77352d275202a3fb4a079dc
rayhun <rayhunkhan27@gmail.com>
2022-11-12 12:20:08 +0600

    update settings

--- QuizApplication/settings.py
+++ QuizApplication/settings.py
@@ -121,9 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR /'static']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-# Media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
